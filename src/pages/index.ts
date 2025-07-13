4fe0b3e05ad89b08c03d300509972bb123dc289e
pedroavv1914 <pedroribeiro1136@gmail.com>
2025-07-13 16:06:12 -0300

    [feat] Add Login, Register, NotFound pages and Navbar component

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -5,4 +5,6 @@ export { default as ProductDetail } from './ProductDetail';
 export { default as Cart } from './Cart';
 export { default as Login } from './Login';
 export { default as Signup } from './Signup';
+export { default as Profile } from './Profile';
+export { default as Orders } from './Orders';
 export { default as NotFound } from './NotFound';
